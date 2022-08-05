29c8d5e03d0c9803bed4d41edbb8537a8036e755
Kanishk <kai7565105@gmail.com>
2022-08-05 18:20:39 +0530

    REST Api Setup complete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,20 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
 
 
 // @ desc - Get goals (right now it is getting all the goals but when we add user authentication it will only get a specific user goal
 // @route - GET/api/goals
 // @access - Private after we add authentication
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get new goals'})
+ const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
+
+
+
+
 // @ desc - Set goal (right now it is getting all the goals but when we add user authentication it will only get a specific user goal
 // @route - POST /api/goals - i.e. you will need to make a POST request
 // @access - Private after we add authentication
@@ -15,20 +23,50 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Set goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    
+    res.status(200).json(goal)
 })
+
+
+
+
 // @ desc - Update goal (right now it is getting all the goals but when we add user authentication it will only get a specific user goal
 // @route - PUT/api/goals/:id (How can you update everything, you can make an update at a specific entry that is why /:id)
 // @access - Private after we add authentication
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true})
+
+    res.status(200).json(updatedGoal)
 })
+
+
+
+
+
 // @ desc - Delete goals (right now it is getting all the goals but when we add user authentication it will only get a specific user goal
 // @route - DELETE/api/goals/:id (How can you update everything, you can make an update at a specific entry that is why /:id)
 // @access - Private after we add authentication
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}`})
+    
+
+    await Goal.remove();
+    
+    res.status(200).json(deleteanyGoal)
 })
+
+
 
 
 // Now to use this we use module.exports
