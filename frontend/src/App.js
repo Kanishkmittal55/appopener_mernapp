3ca25312d26a24dc48e89aa7eed1aa844415e107
Kanishk <kanishkmittal55@gmail.com>
2022-09-01 22:10:15 +0530

    Login Logout Register user complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Login from './features/pages/Login'
-import Register from './features/pages/Register'
-import Dashboard from './features/pages/Dashboard'
-import Header from './components/header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
   return (
     <>
-    <Router>
-    <div className='container'>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Dashboard />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>    
-      </Routes>
-    </div>
-    </Router>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,13 +1,33 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import{Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 
-function header() {
+function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className='header'>
-        <div className="logo">
-            <Link to='/'>GoalSetter</Link>
+        <div className='logo'>
+        <Link to='/'>GoalSetter</Link>
         </div>
         <ul>
+            {user ? (
+                <li>
+                    <button className="btn" onClick={onLogout}>
+                        <FaSignOutAlt /> Logout
+                    </button>
+                </li>
+            ) : ( 
+            <>
             <li>
                 <Link to='/login'>
                     <FaSignInAlt /> Login
@@ -18,9 +38,12 @@ function header() {
                     <FaUser /> Register
                 </Link>
             </li>
+            </>
+            )}
+           
         </ul>
-    </header>
+        </header>
   )
 }
 
-export default header
+export default Header
